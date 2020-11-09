92cfd1bef0db6298f02c58df213af759c0bc4715
Athira.V.Ajit <mailathiravajit@gmail.com>
2020-11-10 00:15:55 +0530

    adding index with animation

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const PORT = 3000
 app.set('view engine','ejs')
 app.use(express.static(path.join(__dirname,'public')))
 
+app.use('/',require('./routes/index.js'))
 app.use('/dashboard',require('./routes/dashboard'))
 
 app.listen(PORT,console.log(`listening to port ${PORT}`))
